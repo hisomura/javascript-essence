c2165bad0b45a02c574c785777c61ae100d36b83
hisomura <hisomura.noreply@gmail.com>
2020-09-15 09:39:42 +0900

    レクチャー109

---
+++ src/iterator.js
@@ -0,0 +1,35 @@
+function genIterator(max = 10) {
+  let i = 0;
+
+  return {
+    next: function () {
+      if (i >= max) {
+        return {
+          done: true,
+        };
+      }
+
+      return {
+        done: false,
+        value: i++,
+      };
+    },
+  };
+}
+
+// const it = genIterator(10)
+// let a = it.next();
+// while (!a.done) {
+//   console.log(a.value);
+//   a = it.next();
+// }
+
+const obj = {
+  [Symbol.iterator]: genIterator
+}
+
+for (const i of obj) {
+  console.log(i)
+}
+
+console.log(new Set(obj))
