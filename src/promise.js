4a95fcb525da2f9d4ac8aea6739a69ac70e8e18e
hisomura <hisomura.noreply@gmail.com>
2020-10-12 09:56:20 +0900

    レクチャー127

--- src/promise.js
+++ src/promise.js
@@ -21,3 +21,19 @@ new Promise(function (resolve, reject) {
 })
 
 console.log('global end')
+
+function sleep(val) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      console.log(val++);
+      resolve(val);
+    }, 1000);
+  });
+}
+
+sleep(0)
+  .then(val => sleep(val))
+  .then(val => sleep(val))
+  .then(val => sleep(val))
+  .then(val => sleep(val))
+  .then(val => sleep(val))
